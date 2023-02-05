c299fbec28ea5dd6760bd1165393c1b2b57deb6a
inhwa-son <patate.douce.pomme@gmail.com>
2023-02-05 15:26:18 -0800

    ExtraCredit1 #2, #3 done

--- src/cs545/ExtraCredit1.java
+++ src/cs545/ExtraCredit1.java
@@ -1,5 +1,7 @@
 package cs545;
 
+import java.util.*;
+
 public class ExtraCredit1 {
     /**
      * 1351. Count Negative Numbers in a Sorted Matrix
@@ -15,6 +17,74 @@ public class ExtraCredit1 {
                 }
             }
         }
+
+        /* Running time for the algorithms: O(n^2) */
         return cnt;
+    }
+
+
+    /**
+     * 1859. Sorting the Sentence
+     * https://leetcode.com/problems/sorting-the-sentence/
+     */
+    public String sortSentence(String s) {
+        // Split the input by blank space
+        String[] words = s.split(" ");
+
+        // Create a return String array with the size of input
+        String[] rtn = new String[words.length];
+
+        // idx is for the input that does not have sequence at the end
+        int idx = 0;
+
+        for (String word : words) {
+
+            // Get the last character
+            String substring = word.substring(word.length()-1, word.length());
+
+            // If the last character is a letter then we have to shuffle
+            if ( Character.isLetter(substring.toCharArray()[0]) ){
+                rtn[idx] = word+(idx+1);
+                idx++;
+            }else{
+                // If the last character is a number, we need to shuffle for original position
+                rtn[Integer.parseInt(substring)-1] = word.substring(0, word.length()-1);
+            }
+
+        }
+
+        StringBuilder rtnVal = new StringBuilder();
+        for (String s1 : rtn) {
+            rtnVal.append(s1).append(" ");
+        }
+
+        /* Running time for the algorithms: O(n) */
+        // We have to remove blank space at the end
+        return rtnVal.toString().substring(0, rtnVal.length()-1);
+    }
+
+    /**
+     * 2418. Sort the People
+     * https://leetcode.com/problems/sort-the-people/
+     */
+    public String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer, String> hm = new HashMap<>();
+        List<Integer> hs = new ArrayList<>();
+
+        int idx = 0;
+        for (String name : names) {
+            hm.put(heights[idx], name);
+            hs.add(idx, heights[idx]);
+            idx++;
+        }
+
+        Collections.sort(hs, Collections.reverseOrder());
+
+        String[] rtn = new String[names.length];
+        for(int i = 0 ; i < hs.size(); i++){
+            rtn[i] = hm.get(hs.get(i));
+        }
+
+        return rtn;
     }
 }
--- src/cs545/ExtraCredit1Test.java
+++ src/cs545/ExtraCredit1Test.java
@@ -6,12 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ExtraCredit1Test {
 
+    ExtraCredit1 extraCredit1 = new ExtraCredit1();
+
     @Test
     void countNegatives() {
-        ExtraCredit1 extraCredit1 = new ExtraCredit1();
-
         System.out.println(
                 extraCredit1.countNegatives(new int[][]{ {4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}})
+        );
+    }
+
+    @Test
+    void sortSentence() {
+        System.out.println(
+                extraCredit1.sortSentence("is2 sentence4 This1 a3")
+        );
+
+    }
+
+    @Test
+    void sortPeople() {
+        System.out.println(
+                extraCredit1.sortPeople(new String[]{"Mary","John","Emma"}, new int[]{180,165,170})
         );
     }
 }
