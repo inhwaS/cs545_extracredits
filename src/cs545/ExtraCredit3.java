7d292a92b937fc257e8cbd560b7104b563607c3a
inhwa-son <patate.douce.pomme@gmail.com>
2023-02-19 12:06:13 -0800

    - ExtraCredit3 #1 figured out another solution

--- src/cs545/ExtraCredit3.java
+++ src/cs545/ExtraCredit3.java
@@ -11,9 +11,36 @@ public class ExtraCredit3 {
      * https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
      */
     public int maxProductDifference(int[] nums) {
-        Arrays.sort(nums);
+        // This is too simple to sort and inefficient to sort
+//        Arrays.sort(nums);
 
-        return  (nums[0] * nums[1]) - (nums[nums.length-2]*nums[nums.length]-1);
+        // Changed based on solution
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for(int val : nums){
+            // Set the first two maximum values
+            if(val > max1){
+                max2 = max1;
+                max1 = val;
+            } else if(val > max2){
+                max2 = val;
+            }
+
+            // Set the first two minimum values
+            if(val < min1){
+                min2 = min1;
+                min1 = val;
+            } else if(val < min2){
+                min2 = val;
+            }
+        }
+
+        return (max1 * max2) - (min1 * min2);
+        /* Time complexity : O(n) */
     }
 
 }
